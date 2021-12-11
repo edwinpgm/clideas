c3601e36393743fab210abf6466f237a0023110c
Edwin <edwinpgm@gmail.com>
2021-12-10 19:25:43 -0500

    Add alerts and icons

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
-const welcome = require('cli-welcome');
-const pkgJSON = require('./package.json');
-const chalk = require('chalk');
+import welcome from 'cli-welcome';
+// import pkgJSON from './package.json';
+import chalk from 'chalk';
+
 const log = console.log;
 const title = chalk.hex('#000000').bgYellow.bold
+// Alerts
+import sym from 'log-symbols'
+const success = chalk.green
+const info = chalk.blue
+const warning = chalk.keyword('orange')
+const error = chalk.red.bold
 
 // Welcome message
 welcome({
-  title: pkgJSON.name,
+  title: `pkgJSON.name`,
   tagLine: 'My ideas',
-  description: pkgJSON.description,
-  version: pkgJSON.version,
+  description: `pkgJSON.description`,
+  version: `pkgJSON.version`,
   bgColor: '#9042f5',
   color: '#ffffff',
   bold: true,
@@ -22,4 +29,9 @@ ${title(` These are my ideas saved `)}
 
 1. asdsad asdasd
 2. asdsad asdasd
+
+${sym.success} ${success(`Success`)}
+${sym.info} ${info(`Info`)}
+${sym.warning} ${warning(`Warning`)}
+${sym.error} ${error(`Error`)}
 `);
